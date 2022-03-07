92dbbb144945781df0079ed100c1ea0fc4cedc7e
Nikhil <nikhilsmanu@gmail.com>
2022-03-08 01:44:37 +0530

    Infiinte Board Solution

--- Array/Array Medium/013-Game of Life.py
+++ Array/Array Medium/013-Game of Life.py
@@ -65,3 +65,45 @@ def outOfBound(board, i, j):
     return i < 0 or j < 0 or i >= len(board) or j >= len(board[i])
 
 
+# Time O(MN) | Space O(A) -- where A is the number of alive nodes
+
+#Infinite Board Solution
+def gameOfLife(board):
+    directions = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+    alive = getAliveIdx(board)
+    
+    count = {}
+    for i, j in alive:
+        for x, y in directions:
+            if (i + x, j + y) not in count:
+                count[(i + x, j + y)] = 0
+            count[(i + x, j + y)] += 1
+            
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if (i, j) not in count:
+                board[i][j] = 0
+                continue
+                
+            if count[(i, j)] == 3 and board[i][j] == 0:
+                board[i][j] = 1
+            if count[(i, j)] not in [2, 3] and board[i][j] == 1:
+                board[i][j] = 0
+                
+    
+def getAliveIdx(board):
+    alive = set()
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j]:
+                alive.add((i, j))
+                
+    return alive
+
+"""
+The Idea in the Last Solution is that the board is infinite.
+
+ -> So we take the points which are alive
+ -> then we keep count of alive for neighbours and the current node
+ -> In the last step, we just find the take the conditions for dead --> alive and vice versa 
+"""
