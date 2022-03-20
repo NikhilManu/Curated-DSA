0a5eb3f322471a2597c08da83e3abc601f63e450
Nikhil <nikhilsmanu@gmail.com>
2022-03-20 23:27:47 +0530

    Array Hard

---
+++ Array/Array Hard/001-First Missing Positive.py
@@ -0,0 +1,32 @@
+# Time O(N) | Space O(1)
+
+def firstMissingPositive(nums):
+    removeNegativeNumbers(nums)
+    markVisitedNumbers(nums)
+            
+    for i, num in enumerate(nums):
+        if num > 0:
+            return i + 1
+        
+    return len(nums) + 1
+    
+def removeNegativeNumbers(nums):
+    for i, num in enumerate(nums):
+        if num <= 0:
+            nums[i] = float('inf')
+        
+def markVisitedNumbers(nums):
+    for num in nums:
+        index = abs(num) - 1
+        if index < len(nums):
+            nums[index] = -abs(nums[index])
+
+
+"""
+Intution: 
+    The Possible answers are only in range (1, n + 1) where n -> length of the array
+
+    => So firstly we dont care about the negative numbers, so we remove them
+    => Then, we are making index to negative if we visit any number. (if we get a number 3, then we will make index 2 to negative)
+    => Now iterate throught the array and find the first index with positive number
+"""
