31d6e422b2cf5d5fa56327693d56ecbb6727a506
Nikhil <nikhilsmanu@gmail.com>
2022-03-23 23:37:10 +0530

    Added Optimized Solution

--- Array/Array Medium/003-Array of Product Except Self.py
+++ Array/Array Medium/003-Array of Product Except Self.py
@@ -1,5 +1,6 @@
 # Time O(N) | Space O(N)
 
+# Two Pass
 def productExceptSelf(nums):
     res = [1] * len(nums)
     leftRunningProduct = 1
@@ -11,5 +12,17 @@ def productExceptSelf(nums):
     for i in reversed(range(len(nums) - 1)):
         rightRunningProduct *= nums[i + 1]
         res[i] *= rightRunningProduct
+
+    return res
+
+# Single Pass
+def productExceptSelf(nums):
+    res = [1] * len(nums)
+    leftRunningProduct, rightRunningProduct = 1, 1
+    for i in range(len(nums)):
+        res[i] *= leftRunningProduct
+        leftRunningProduct *= nums[i]
+        res[len(nums) - i - 1] *= rightRunningProduct
+        rightRunningProduct *= nums[len(nums) - i - 1]
 
     return res
