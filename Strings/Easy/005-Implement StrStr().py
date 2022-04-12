d382125fa41c538f85583c105983d580ab482a73
Nikhil <nikhilsmanu@gmail.com>
2022-04-13 01:54:30 +0530

    String Easy

--- Strings/Easy/005-Implement StrStr().py
+++ Strings/Easy/005-Implement StrStr().py
@@ -10,3 +10,40 @@ def strStr(haystack, needle):
             if j == len(needle): return idx
             
     return -1
+
+# Knuth Morris Pratt Algorithm  --> https://www.youtube.com/watch?v=JoF0Z7nVSrA
+# Time O(N + M) | Space O(M)
+
+def strStr(haystack, needle):
+    pattern = buildLongestPrefixSuffixArray(needle)
+    return KMP(haystack, needle, pattern)
+    
+def buildLongestPrefixSuffixArray(needle):
+    prev, cur = 0, 1
+    pattern = [0] * len(needle)
+    while cur < len(needle):
+        if needle[cur] == needle[prev]:
+            pattern[cur] = prev + 1
+            prev += 1; cur += 1
+        elif prev == 0:
+            pattern[cur] = 0
+            cur += 1
+        else:
+            prev = pattern[prev - 1]
+            
+    return pattern 
+
+def KMP(haystack, needle, pattern):
+    i, j = 0, 0
+    while i < len(haystack):
+        if haystack[i] == needle[j]:
+            i += 1; j += 1
+        elif j == 0:
+            i += 1
+        else:
+            j = pattern[j - 1]
+            
+        if j == len(needle):
+            return i - len(needle)
+        
+    return -1
